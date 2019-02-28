db7e805794e825e2c152ee2aa550c30da775f9fa
vsnappy1 <vsnappy1@gmail.com>
2019-02-28 16:58:35 +0500

    No need to input number of elements in Alphabet just enter elements of Alphabet as a string separated by ','

--- main.c
+++ main.c
@@ -1,13 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+//Vishal B16101182 Assignment-1
+//Objective: Write a program to input an Alphabet and then determine whether given Alphabet is valid or invalid
 
 int main()
 {
-    int i=0,isTrue=1;
-
-    char *A[] = {"B", "Ba", "bab", "d"};        //Declaring Alphabet
+    int i=0,j=0,n,ArrayLength,isTrue=1;
     char First_Char , Check_Char;
-    int  ArrayLength = sizeof(A)/8;             //Size of Array
+    char str1[50];
+    printf("Enter Valid Alphabet = { ");
+	scanf("%s",str1);					        //this takes string input Alphabet seperated by ,
+	int count=1;
+
+	for( j=0;j<strlen(str1);j++){		        //This counts the number of elements in Alphabet
+		if(str1[j]==','){
+			count++;
+		}
+	}
+	n=count;
+
+	// Returns first token
+    char* token = strtok(str1, ",");
+
+  	char *A[n];
+    while (token != NULL) { 				    //This fills the array with elements
+    	A[i]=(char*)malloc(10);
+    	strcpy (A[i], token);
+        token = strtok(NULL, ",");
+        i++;
+    }
+    ArrayLength = sizeof(A)/8;                  //Size of Array
 
     for(i=0;i<ArrayLength;i++)                  //This is main loop which picks a letter
         {
@@ -29,9 +53,9 @@ int main()
         }
 
    if(isTrue==1)
-       printf("Valid Alphabet");
+       printf("\nValid Alphabet\n");
    else
-      printf("Invalid Alphabet");
+      printf("\nInvalid Alphabet\n");
 
     return 0;
 
